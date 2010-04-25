49ddcdf70108527ecdda4f10afac8acd6394444c
evils.mail <evils.mail@5ca492cb-e106-f94d-691b-a09dbeb2803d>
2010-04-25 18:45:51 +0000

    Create BitmapService.cs

---
+++ trunk/rocognitionofhumanbyretina/common/BitmapService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.IO;
+
+namespace rocognitionofhumanbyretina.common
+{
+    class BitmapService
+    {
+        public byte[] bmpToByte(Bitmap image)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            image.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Bmp);
+            return memoryStream.GetBuffer();
+        }
+
+        public Bitmap ByteToBmp(byte[] imgByteArray)
+        {
+            return new Bitmap(new MemoryStream(imgByteArray));
+        }
+    }
+}
