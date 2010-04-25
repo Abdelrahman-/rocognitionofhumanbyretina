8b249a030a1891104be70fbd14dce39f79c1d92f
evils.mail <evils.mail@5ca492cb-e106-f94d-691b-a09dbeb2803d>
2010-04-25 18:02:09 +0000

    AddForm refactoring

--- trunk/rocognitionofhumanbyretina/AddForm.cs
+++ trunk/rocognitionofhumanbyretina/AddForm.cs
@@ -15,5 +15,31 @@ namespace rocognitionofhumanbyretina
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(openFileDialog1.FileName);
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox2.Image = new Bitmap(openFileDialog1.FileName);
+            }
+        }
     }
 }
