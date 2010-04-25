3e47600e6dbfa953eb3766dd68f32fd06bd2039c
evils.mail <evils.mail@5ca492cb-e106-f94d-691b-a09dbeb2803d>
2010-04-25 14:10:11 +0000

    Create database

---
+++ rocognitionofhumanbyretina/DB/Connector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace rocognitionofhumanbyretina.DB
+{
+    class Connector
+    {
+        private SqlConnection sqlConnection = new SqlConnection();
+        private SqlCommand sqlCommand = new SqlCommand();
+        private SqlDataAdapter dataAdapter = new SqlDataAdapter();
+
+        public void init()
+        {
+            
+            DatabaseDataSet dataSet = new DatabaseDataSet();
+            dataAdapter.SelectCommand = sqlCommand;
+            dataAdapter.Fill(dataSet, "test");
+            DataTable dataTable = dataSet.Tables["test"];
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                MessageBox.Show("Id: " + dataRow["id"]);
+            }
+        }
+    }
+}
