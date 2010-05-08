6314beef151a3c67bba27943aec61ff4d95b4d79
evils.mail <evils.mail@5ca492cb-e106-f94d-691b-a09dbeb2803d>
2010-05-08 22:41:58 +0000

    Add Gabor2D filter

---
+++ rocognitionofhumanbyretina/common/enums/EyeTypes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace rocognitionofhumanbyretina.common.enums
+{
+    enum EyeTypes 
+    {
+        LEFT, 
+        RIGHT
+    }
+}
